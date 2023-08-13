afccd7fc8929c886fdbac1b6d434a1e8b0d7a7f3
AkhilReddyGundla <121927997+AkhilReddyGundla@users.noreply.github.com>
2023-08-13 16:59:17 +0530

    script.js
    used another form of sending email using emailjs.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const toggleButton = document.getElementById('nav-toggle');
     const navLinks = document.getElementById('nav-links');
@@ -11,24 +13,28 @@ document.addEventListener('DOMContentLoaded', () => {
         navLinks.classList.toggle('active');
     });
 });
-
-
-function sendEmail(){
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("mail").value;
-    let message = document.getElementById("message").value;
-    let body="Name :" + name + "<br/>Email : "+ email + "<br/>Body : "+message;
-    email.send({
-        SecureToken : "0e1fd588-edac-4e07-a636-4ba0121944ca",
-        To : "akhilreddygundla2@gmail.com",
-        From : "akhilreddygundla@gmail.com",
-        Subject : "New message",
-        Body : body,
-    }).then(
-    message => alert(message)
-    );
-}
-
+document.addEventListener('DOMContentLoaded', () => {
+    const submitButton = document.querySelector('.form_submit-btn');
+    submitButton.addEventListener('click',function(event){
+        event.preventDefault();
+        let parameters = {
+            name : document.getElementById("name").value,
+            designation : document.getElementById("designation").value,
+            message : document.getElementById("message").value,
+        };
+        const serviceId = "service_9kwgobl";
+        const templetId = "template_4blntos";
+        emailjs.init("mo02Cwj35purVWKKt");
+        emailjs
+            .send(serviceId,templetId,parameters).then((res)=>{
+                document.getElementById("name").value="";
+                document.getElementById("designation").value="";
+                document.getElementById("message").value="";
+                alert("Your message sent successfully");
+            })
+            .catch((err) =>console.log(err));
+    })
+});
 
 
 
@@ -70,7 +76,7 @@ document.addEventListener('DOMContentLoaded', () => {
   document.addEventListener('DOMContentLoaded',()=>{
     const resume=document.querySelector('#resume');
     resume.addEventListener('click',(e)=>{
-        window.open("documents/akhil'sResume.pdf", '_blank');
+        window.open('documents/finalResumeCMV.pdf', '_blank');
     })
   })
   
