026a15f33e843329a4404f035abe5340da98967f
AkhilReddyGundla <121927997+AkhilReddyGundla@users.noreply.github.com>
2023-08-14 10:01:05 +0530

    script.js

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ document.addEventListener('DOMContentLoaded', () => {
   document.addEventListener('DOMContentLoaded',()=>{
     const resume=document.querySelector('#resume');
     resume.addEventListener('click',(e)=>{
-        window.open('documents/finalResumeCMV.pdf', '_blank');
+        window.open("documents/akhil'sResume", '_blank');
     })
   })
   
